234143d90bbf7d1b2cfe058048ab2b46c5cd1f16
Stephan Schober <teh.fonsi@gmail.com>
2016-01-05 15:23:06 +0100

    added additional conditions and comments

--- library/src/main/java/tehfonsi/github/com/appvirality/AbstractBuilder.java
+++ library/src/main/java/tehfonsi/github/com/appvirality/AbstractBuilder.java
@@ -3,43 +3,82 @@ package tehfonsi.github.com.appvirality;
 import android.content.Context;
 import android.util.Log;
 
-import tehfonsi.github.com.appvirality.preferences.AppViralityPreferences;
-import tehfonsi.github.com.appvirality.preferences.RatingPreferences;
-
 /**
  * Created by stephanschober on 31.12.15.
  */
 public class AbstractBuilder {
 
     private final String TAG  = AbstractBuilder.class.getSimpleName();
-    private RatingPreferences mPreferences;
+    private AppViralityPreferences mPreferences;
 
     private int mTimesShown = -1;
     private int mMinStartCount = 3;
     private int mMinStartCountWithoutCrash = 3;
     private String mFeedbackMail;
+    private CustomCondition mAdditionalCustomCondition;
+    private CustomCondition mCustomOnlyCondition;
 
-    public AbstractBuilder(Context mContext) {
-        this.mPreferences = new RatingPreferences(mContext);
+    public AbstractBuilder(Context mContext, AppViralityPreferences preferences) {
+        this.mPreferences = preferences;
 
         initExceptionHandler();
     }
 
+    /**
+     * Add an additional condition which will be checked along the default conditions
+     * @param customCondition
+     * @return
+     */
+    public AbstractBuilder withAdditionalCustomCondition(CustomCondition customCondition) {
+        this.mAdditionalCustomCondition = customCondition;
+        return this;
+    }
+
+    /**
+     * This is the only condition which will be checked
+     * @param customCondition
+     * @return
+     */
+    public AbstractBuilder withCustomConditionOnly(CustomCondition customCondition) {
+        this.mCustomOnlyCondition = customCondition;
+        return this;
+    }
+
+    /**
+     * Define the minimum number of starts before anything is shown
+     * @param minStartCount
+     * @return
+     */
     public AbstractBuilder withMinStartCount(int minStartCount) {
         this.mMinStartCount = minStartCount;
         return this;
     }
 
+    /**
+     * Define the minimum number of starts, without a preceding crash, before anything is shown
+     * @param minStartCountWithoutCrash
+     * @return
+     */
     public AbstractBuilder withMinStartCountWithoutCrash(int minStartCountWithoutCrash) {
         this.mMinStartCountWithoutCrash = minStartCountWithoutCrash;
         return this;
     }
 
+    /**
+     * Define how often anything should be shown, default is indefinite
+     * @param timesShown
+     * @return
+     */
     public AbstractBuilder withTimesShown(int timesShown) {
         this.mTimesShown = timesShown;
         return this;
     }
 
+    /**
+     * Define a feedback mail, only used for rating/feedback logic
+     * @param feedbackMail
+     * @return
+     */
     public AbstractBuilder withFeedbackMail(String feedbackMail) {
         this.mFeedbackMail = feedbackMail;
         return this;
@@ -56,35 +95,56 @@ public class AbstractBuilder {
         }
     }
 
+    /**
+     * Check if all conditions are met
+     * @return
+     */
     public boolean shouldShow() {
+        if (mCustomOnlyCondition != null) {
+            return mCustomOnlyCondition.shouldShow();
+        }
+
         boolean startCountCondition = false;
         boolean startCountWithoutCrashCondition = false;
         boolean timesShownCondition = false;
+        boolean customCondition = false;
 
         if (mPreferences.getStartCount() >= mMinStartCount) {
-            Log.v(TAG, "startCountCondition");
+            Log.v(TAG, mPreferences.getPreferenceName()+": startCountCondition");
             startCountCondition = true;
         }
 
         if (mPreferences.getStartCountWithoutCrash() >= mMinStartCountWithoutCrash) {
-            Log.v(TAG, "startCountWithoutCrashCondition");
+            Log.v(TAG, mPreferences.getPreferenceName()+": startCountWithoutCrashCondition");
             startCountWithoutCrashCondition = true;
         }
 
         if (mPreferences.getTimesShown() < mTimesShown || mTimesShown < 0) {
-            Log.v(TAG, "timesShownCondition");
+            Log.v(TAG, mPreferences.getPreferenceName()+": timesShownCondition");
             timesShownCondition = true;
         }
 
-        return startCountCondition && startCountWithoutCrashCondition && timesShownCondition;
+        if (mAdditionalCustomCondition == null) {
+            customCondition = true;
+        } else {
+            customCondition = mAdditionalCustomCondition.shouldShow();
+            Log.v(TAG, mPreferences.getPreferenceName()+": additionalCustomCondition: "+customCondition);
+        }
+
+        return startCountCondition && startCountWithoutCrashCondition && timesShownCondition && customCondition;
     }
 
+    /**
+     * Increase the number of starts
+     *
+     * see {@link #withMinStartCount(int) withMinStartCount} and {@link #withMinStartCountWithoutCrash(int) withMinStartCountWithoutCrash}
+     */
     public void increaseStartCount() {
         mPreferences.increaseStartCount();
         mPreferences.increaseStartCountWithoutCrash();
     }
 
-    protected RatingPreferences getPreferences() {
+    protected AppViralityPreferences getPreferences() {
         return mPreferences;
     }
 
@@ -94,6 +154,10 @@ public class AbstractBuilder {
 
     public void reset() {
         mPreferences.resetAll();
+    }
+
+    public interface CustomCondition {
+        boolean shouldShow();
     }
 
 }
